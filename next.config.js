29b0bc0be559e74347691a2135cb9efa3270114c
Tony Cheong <tony.cheong@anacle.com>
2024-01-23 18:03:53 +0800

    Add Next.js bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: [ 'daisyui.com', 'i.imgur.com' ],
-        unoptimized: true
-    },
-    output: 'export',
-}
+  images: {
+    domains: ["daisyui.com", "i.imgur.com"],
+    unoptimized: true,
+  },
+  output: "export",
+};
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig);
